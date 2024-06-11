f682946287c475f1fd2bbecadfe8e66a309f037c
brandonejhall <brandonejhall7@outlook.com>
2024-06-11 05:49:07 -0500

    signup-controller

---
+++ usermanagement/src/main/java/com/swiftrecruit/usermanagement/controller/AuthController.java
@@ -0,0 +1,59 @@
+package com.swiftrecruit.usermanagement.controller;
+
+import jakarta.validation.Valid;
+import com.swiftrecruit.usermanagement.dto.UserDto;
+import com.swiftrecruit.usermanagement.entity.User;
+import com.swiftrecruit.usermanagement.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AuthController {
+
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // handler method to handle home page request
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+
+    // handler method to handle user registration form submit request
+    @PostMapping("/register")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+            BindingResult result,
+            Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
+
+    // handler method to handle list of users
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
